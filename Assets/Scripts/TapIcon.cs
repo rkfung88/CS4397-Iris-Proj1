d1fa3e5d5aabbba1415d605294ff7211c03ad6e7
Trixie Tram Nguyen <rtq.nguyen12@gmail.com>
2021-03-18 18:10:44 -0500

    Tried to make touch, zoom and move using fingers.

---
+++ Assets/Scripts/TapIcon.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapIcon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                Debug.Log("Something Touch");
+                if (raycastHit.collider.name == "Map")
+                {
+                    Debug.Log("Map clicked");
+                }
+
+                //OR with Tag
+
+                //if (raycastHit.collider.CompareTag("SoccerTag"))
+                //{
+                //    Debug.Log("Soccer Ball clicked");
+                //}
+            }
+        }
+    }
+}
