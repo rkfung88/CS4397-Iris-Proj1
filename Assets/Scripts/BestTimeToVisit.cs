5df88deaac0682f394f743a6a7cc2261faeb73ee
dvillarreal54 <dvillarreal54@gmail.com>
2021-03-22 03:10:16 -0500

    Update BestTimeToVisit.cs

--- Assets/Scripts/BestTimeToVisit.cs
+++ Assets/Scripts/BestTimeToVisit.cs
@@ -8,32 +8,56 @@ using TMPro;
 
 public class BestTimeToVisit : MonoBehaviour
 {
+    private InfoManager infomanager;
+    public TextMeshPro city;
     MongoClient client = new MongoClient("mongodb+srv://dvillarreal54:v0808180@cluster0.tgnzx.mongodb.net/Location_Info?retryWrites=true&w=majority");
     IMongoDatabase database;
     IMongoCollection<BsonDocument> collection;
-    private TextMeshPro BTVOutput;
+    //private TextMeshPro fOutput;
     // Start is called before the first frame update
     void Start()
     {
-        BTVOutput = GetComponentInChildren<TextMeshPro>();
-
+        infomanager = FindObjectOfType<InfoManager>();
         database = client.GetDatabase("Location_Info");
         collection = database.GetCollection<BsonDocument>("Weather_BestTimeToVisit");
+    }
 
-        var filter = Builders<BsonDocument>.Filter.Eq("Location", "Seoul");
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            infomanager.UpdateIconVisibility(transform.name);
+            var filter = Builders<BsonDocument>.Filter.Eq("Location", city.text);
+            var studentDocument = collection.Find(filter).FirstOrDefault();
+            string temp = studentDocument.ToString();
+
+            var stringWoId = temp.Substring(temp.IndexOf("),") + 4);
+            string stringWoLoc = stringWoId.Substring(stringWoId.IndexOf(",") + 3);
+            string BTV = stringWoLoc.Substring(stringWoLoc.IndexOf(",") + 3);
+            string Festivities = BTV.Substring(BTV.IndexOf(",") + 3);
+            string BTV2 = BTV.Substring(0, BTV.IndexOf(","));
+            string Festivities2 = Festivities.Substring(0, Festivities.IndexOf(","));
+
+            Debug.Log(BTV2);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        infomanager.UpdateIconVisibility(transform.name);
+        var filter = Builders<BsonDocument>.Filter.Eq("Location", city.text);
         var studentDocument = collection.Find(filter).FirstOrDefault();
         string temp = studentDocument.ToString();
 
         var stringWoId = temp.Substring(temp.IndexOf("),") + 4);
         string stringWoLoc = stringWoId.Substring(stringWoId.IndexOf(",") + 3);
         string BTV = stringWoLoc.Substring(stringWoLoc.IndexOf(",") + 3);
+        string Festivities = BTV.Substring(BTV.IndexOf(",") + 3);
         string BTV2 = BTV.Substring(0, BTV.IndexOf(","));
-        BTVOutput.text = BTV2;
-    }
+        string Festivities2 = Festivities.Substring(0, Festivities.IndexOf(","));
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log(BTV2);
+
     }
 }
