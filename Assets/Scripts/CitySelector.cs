2ff8a46570a46c53865789a851665e2608958520
Trixie Tram Nguyen <rtq.nguyen12@gmail.com>
2021-04-29 14:01:33 -0500

    Tap the pin works

--- Assets/Scripts/CitySelector.cs
+++ Assets/Scripts/CitySelector.cs
@@ -16,27 +16,35 @@ public class CitySelector : MonoBehaviour
     {
         if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
         {
-            //infomanager.UpdateName(transform.name);
-            //infomanager.UpdateVisibility(true);
-            //FinalOutput.text = " ";
-            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            RaycastHit raycastHit;
-            if (Physics.Raycast(raycast, out raycastHit))
+            if (Input.touchCount == 1)
             {
-                Debug.Log("010");
-                //    //if (raycastHit.collider.name == "Map")
-                //    //{
-                //    //    Debug.Log("Map clicked");
-                //    //}
-
-                //    //OR with Tag
-
-                if (raycastHit.collider.CompareTag("Tokyo"))
+                Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                Vector2 touchPos = new Vector2(wp.x, wp.y);
+                if (GetComponent<Collider2D>().OverlapPoint(wp))
                 {
-                    Debug.Log("Tokyo");
-                    infomanager.UpdateName(transform.name);
-                    infomanager.UpdateVisibility(true);
-                    FinalOutput.text = " ";
+                    //infomanager.UpdateName(transform.name);
+                    //infomanager.UpdateVisibility(true);
+                    //FinalOutput.text = " ";
+                    Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                    RaycastHit raycastHit;
+                    if (Physics.Raycast(raycast, out raycastHit))
+                    {
+                        Debug.Log("010");
+                        //    //if (raycastHit.collider.name == "Map")
+                        //    //{
+                        //    //    Debug.Log("Map clicked");
+                        //    //}
+
+                        //    //OR with Tag
+
+                        if (raycastHit.collider.CompareTag("Tokyo"))
+                        {
+                            Debug.Log("Tokyo");
+                            infomanager.UpdateName(transform.name);
+                            infomanager.UpdateVisibility(true);
+                            FinalOutput.text = " ";
+                        }
+                    }
                 }
             }
         }
